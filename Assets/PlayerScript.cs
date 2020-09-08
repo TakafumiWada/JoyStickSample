3f12ee99b099de757dfcb370c8eadf5c04a86019
WadaTakafumi <wdtk2525@gmail.com>
2020-09-08 14:00:50 +0900

    change speed scope

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,7 +6,7 @@ public class PlayerScript : MonoBehaviour
 {
     public FloatingJoystick right;
     public FloatingJoystick left;
-    float speed=20;
+    public float speed;
     float rot=5;
     // Start is called before the first frame update
     void Start()
@@ -17,9 +17,9 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         //左スティックでの縦移動
-        this.transform.position += this.transform.forward * right.Vertical * Time.deltaTime;
+        this.transform.position += this.transform.forward * right.Vertical *speed* Time.deltaTime;
         //左スティックでの横移動
-        this.transform.position += this.transform.right * right.Horizontal * Time.deltaTime; 
+        this.transform.position += this.transform.right * right.Horizontal * speed*Time.deltaTime; 
         //右スティックでの回転
         transform.Rotate(new Vector3(0,rot*left.Horizontal,0));
     }
